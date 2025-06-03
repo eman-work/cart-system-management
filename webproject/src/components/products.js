671a9cf6e131ad4364379e9958f57b4481ba20a7
eman-work <emanasghar.work@gmail.com>
2025-06-04 04:42:10 +0500

    myproj

---
+++ webproject/src/components/products.js
@@ -0,0 +1,63 @@
+import imageProduct1 from '../assets/images/1.jpg'
+import imageProduct2 from '../assets/images/2.jpg'
+import imageProduct3 from '../assets/images/3.jpg'
+import imageProduct4 from '../assets/images/4.jpg'
+import imageProduct5 from '../assets/images/5.jpg'
+import imageProduct6 from '../assets/images/6.jpg'
+
+export const products =[
+    {
+        id:1,
+        name:'Red roses bouqet',
+        price:2000,
+        image:imageProduct1,
+        description:'This bouqet includes fresh Red roses x9 wrapped up in a bouqet',
+        slug:'red-roses-bouqet'
+
+    },
+        {
+        id:2,
+        name:'flowers x ferrrero bouqet',
+        price:6000,
+        image:imageProduct2,
+        description:'This bouqet includes fresh white roses x9 ferrero rochers x12 baby breaths wrapped up in a bouqet',
+        slug:'flowersxferrrerobouqet'
+
+    },
+        {
+        id:3,
+        name:'ferrero bouqet',
+        price:4500,
+        image:imageProduct3,
+        description:'This bouqet includes ferrero rochers x9 with baby breaths and a happy birthday cake topper wrapped up in a bouqet',
+        slug:'ferrerobouqet'
+
+    },
+        {
+        id:4,
+        name:'flowers x breuno bouqet',
+        price:7000,
+        image:imageProduct4,
+        description:'This bouqet includes fresh Red roses x6 breuno chocolate bars x6 kinder0joy x2 wrapped up ina bouqet',
+        slug:'flowersxbreunobouqet'
+
+    },
+            {
+        id:5,
+        name:'ferrerohappybirthday',
+        price:4300,
+        image:imageProduct5,
+        description:'This bouqet includes ferrero rochers x9 and a happy birthday cake topper wrapped up in a bouqet',
+        slug:'ferrerohappybirthday'
+
+    },
+            {
+        id:6,
+        name:'rosesxferreroxteadybouqet',
+        price:7000,
+        image:imageProduct6,
+        description:'This bouqet includes fresh red roses x15 ferrero rochers x9 with teady bear wrapped up in a bouqet',
+        slug:'rosesxferreroxteadybouqet'
+
+    }
+];
